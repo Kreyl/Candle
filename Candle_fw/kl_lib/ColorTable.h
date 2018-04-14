ce18e526d057bba51b8f7f440e0404123bd2e88b
Kreyl <laystoll@yandex.ru>
2018-04-14 20:51:47 +0300

    GetPrev in ColorTable

--- Candle_fw/kl_lib/ColorTable.h
+++ Candle_fw/kl_lib/ColorTable.h
@@ -16,10 +16,14 @@ struct ColorTable_t {
     const uint32_t Count;
     const Color_t *Clr;
     const Color_t* GetNext() {
-        const Color_t *Rslt = &Clr[Indx];
         Indx++;
         if(Indx >= Count) Indx=0;
-        return Rslt;
+        return &Clr[Indx];
+    }
+    const Color_t* GetPrev() {
+        if(Indx == 0) Indx = Count-1;
+        else Indx--;
+        return &Clr[Indx];
     }
     const Color_t* GetCurrent() { return &Clr[Indx]; }
     ColorTable_t(const Color_t *ptr, const uint32_t ACnt) :
